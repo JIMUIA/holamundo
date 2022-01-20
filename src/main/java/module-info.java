1e80241c276253038d5fc7831b274e55391ca62f
JuanMelendez <a2209872@correo.uia.mx>
2022-01-19 22:44:58 -0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module uia.arqsoft.holamundo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires javafx.graphics;
+
+    opens uia.arqsoft.holamundo to javafx.fxml;
+    exports uia.arqsoft.holamundo;
+}
